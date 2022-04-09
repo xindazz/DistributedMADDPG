a650629fd597cdf9f18e758762a5f4976529eead
Xinda Zhou <xindaz@weh5201-b6.andrew.cmu.edu>
2022-04-09 18:42:35 -0400

    Minor changes in master

--- master.py
+++ master.py
@@ -6,11 +6,10 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import celery
-from kombu import Queue
+
 from agent import Agent
 from common.replay_buffer import Buffer
 from worker import app, init_agent, NumpyEncoder
-from celery.result import AsyncResult
 
 
 class Runner:
@@ -31,82 +30,85 @@ class Runner:
         # upload params to agents, queue name is "q{agent_id}:
         print("Start init all agents")
 
+        tasks = []
         for i in range(2):
         # for i in range(self.args.n_agents):
             task = app.send_task("worker.init_agent", queue='q' + str(i), kwargs={"agent_id": i, "args": vars(self.args)}, cls=NumpyEncoder)
-            print(task.get())
+            tasks.append(task)
 
-        print("Init all agents done")
+        print("Response: ")
+        for task in tasks:
+            print(task.get())
 
 
     def run(self):
-        pass
-    #     returns = []
-    #     for time_step in tqdm(range(self.args.time_steps)):
-    #         # reset the environment
-    #         if time_step % self.episode_limit == 0:
-    #             s = self.env.reset()
+        # pass
+        returns = []
+        for time_step in tqdm(range(self.args.time_steps)):
+            # reset the environment
+            if time_step % self.episode_limit == 0:
+                s = self.env.reset()
 
-    #         # u contains the actions of the agents that we are training
-    #         u = []
-    #         # actions contains actions of all agents in the environment, including those on the opposing team
-    #         actions = []
+            # u contains the actions of the agents that we are training
+            u = []
+            # actions contains actions of all agents in the environment, including those on the opposing team
+            actions = []
 
-    #         # Get action from every agent
-    #         tasks = []
-    #         data = {}
-    #         for agent_id, agent in enumerate(self.agents):
-    #             tasks.append(get_action.s(json_dump = json.dumps(data, cls=NumpyEncoder)))
+            # Get action from every agent
+            tasks = []
+            data = {}
+            for agent_id, agent in enumerate(self.agents):
+                tasks.append(get_action.s(json_dump = json.dumps(data, cls=NumpyEncoder)))
 
-    #         response = group(tasks)()
-    #         results = response.get()
+            response = group(tasks)()
+            results = response.get()
             
-    #         for result in results:
-    #             u.append(result["action"])
-    #             actions.append(result["action"])
+            for result in results:
+                u.append(result["action"])
+                actions.append(result["action"])
 
-    #         for i in range(self.args.n_agents, self.args.n_players):
-    #             actions.append([0, np.random.rand() * 2 - 1, 0, np.random.rand() * 2 - 1, 0])
-    #         s_next, r, done, info = self.env.step(actions)
-    #         self.buffer.store_episode(s[:self.args.n_agents], u, r[:self.args.n_agents], s_next[:self.args.n_agents])
-    #         s = s_next
-    #         if self.buffer.current_size >= self.args.batch_size:
-    #             transitions = self.buffer.sample(self.args.batch_size)
-    #             for agent in self.agents:
-    #                 other_agents = self.agents.copy()
-    #                 other_agents.remove(agent)
-    #                 agent.learn(transitions, other_agents)
-    #         if time_step > 0 and time_step % self.args.evaluate_rate == 0:
-    #             returns.append(self.evaluate())
-    #             plt.figure()
-    #             plt.plot(range(len(returns)), returns)
-    #             plt.xlabel('episode * ' + str(self.args.evaluate_rate / self.episode_limit))
-    #             plt.ylabel('average returns')
-    #             plt.savefig(self.save_path + '/plt.png', format='png')
-    #             np.save(self.save_path + '/returns.pkl', returns)
-    #         self.noise = max(0.05, self.noise - 0.0000005)
-    #         self.epsilon = max(0.05, self.epsilon - 0.0000005)
-    #         # np.save(self.save_path + '/returns.pkl', returns)
+            for i in range(self.args.n_agents, self.args.n_players):
+                actions.append([0, np.random.rand() * 2 - 1, 0, np.random.rand() * 2 - 1, 0])
+            s_next, r, done, info = self.env.step(actions)
+            self.buffer.store_episode(s[:self.args.n_agents], u, r[:self.args.n_agents], s_next[:self.args.n_agents])
+            s = s_next
+            if self.buffer.current_size >= self.args.batch_size:
+                transitions = self.buffer.sample(self.args.batch_size)
+                for agent in self.agents:
+                    other_agents = self.agents.copy()
+                    other_agents.remove(agent)
+                    agent.learn(transitions, other_agents)
+            if time_step > 0 and time_step % self.args.evaluate_rate == 0:
+                returns.append(self.evaluate())
+                plt.figure()
+                plt.plot(range(len(returns)), returns)
+                plt.xlabel('episode * ' + str(self.args.evaluate_rate / self.episode_limit))
+                plt.ylabel('average returns')
+                plt.savefig(self.save_path + '/plt.png', format='png')
+                np.save(self.save_path + '/returns.pkl', returns)
+            self.noise = max(0.05, self.noise - 0.0000005)
+            self.epsilon = max(0.05, self.epsilon - 0.0000005)
+            # np.save(self.save_path + '/returns.pkl', returns)
 
-    # def evaluate(self):
-    #     returns = []
-    #     for episode in range(self.args.evaluate_episodes):
-    #         # reset the environment
-    #         s = self.env.reset()
-    #         rewards = 0
-    #         for time_step in range(self.args.evaluate_episode_len):
-    #             if self.args.render:
-    #                 self.env.render()
-    #             actions = []
-    #             with torch.no_grad():
-    #                 for agent_id, agent in enumerate(self.agents):
-    #                     action = agent.select_action(s[agent_id], 0, 0)
-    #                     actions.append(action)
-    #             for i in range(self.args.n_agents, self.args.n_players):
-    #                 actions.append([0, np.random.rand() * 2 - 1, 0, np.random.rand() * 2 - 1, 0])
-    #             s_next, r, done, info = self.env.step(actions)
-    #             rewards += r[0]
-    #             s = s_next
-    #         returns.append(rewards)
-    #         print('Returns is', rewards)
-    #     return sum(returns) / self.args.evaluate_episodes
+    def evaluate(self):
+        returns = []
+        for episode in range(self.args.evaluate_episodes):
+            # reset the environment
+            s = self.env.reset()
+            rewards = 0
+            for time_step in range(self.args.evaluate_episode_len):
+                if self.args.render:
+                    self.env.render()
+                actions = []
+                with torch.no_grad():
+                    for agent_id, agent in enumerate(self.agents):
+                        action = agent.select_action(s[agent_id], 0, 0)
+                        actions.append(action)
+                for i in range(self.args.n_agents, self.args.n_players):
+                    actions.append([0, np.random.rand() * 2 - 1, 0, np.random.rand() * 2 - 1, 0])
+                s_next, r, done, info = self.env.step(actions)
+                rewards += r[0]
+                s = s_next
+            returns.append(rewards)
+            print('Returns is', rewards)
+        return sum(returns) / self.args.evaluate_episodes
