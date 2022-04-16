8046be1b9c9157f3a73160a6d9b480f520a71f92
xindazz <xindaz@andrew.cmu.edu>
2022-04-16 05:11:49 -0400

    Debugging rummer_mp

--- runner_mp.py
+++ runner_mp.py
@@ -38,12 +38,13 @@ class Runner:
     def run(self):
         comm = MPI.COMM_WORLD
         size = comm.Get_size()
+        print("Size", size)
         rank = comm.Get_rank()
 
-        s = [np.empty(self.args.obs_shape[agent_id], dtype=np.float64) for agent_id in range(self.args.n_agents)]
-        o_next = [np.empty(self.args.obs_shape[agent_id], dtype=np.float64) for agent_id in range(self.args.n_agents)]
-        u = [np.empty(self.args.action_shape[agent_id], dtype=np.float64) for agent_id in range(self.args.n_agents)]
-        u_next = [np.empty(self.args.action_shape[agent_id], dtype=np.float64) for agent_id in range(self.args.n_agents)]
+        s = [np.empty(self.args.obs_shape[agent_id]) for agent_id in range(self.args.n_agents)]
+        o_next = [np.empty(self.args.obs_shape[agent_id]) for agent_id in range(self.args.n_agents)]
+        u = [np.empty(self.args.action_shape[agent_id]) for agent_id in range(self.args.n_agents)]
+        u_next = [np.empty(self.args.action_shape[agent_id]) for agent_id in range(self.args.n_agents)]
 
         # Master
         if rank == 0:
@@ -55,17 +56,27 @@ class Runner:
                 
                 # actions contains actions of all agents in the environment, including those on the opposing team
                 actions = []
-
-                # Get action from every agent
-                for agent_id in range(self.args.n_agents):
-                    # task = app.send_task("worker.get_action", queue='q' + str(agent_id), kwargs={"agent_id": agent_id, "args": vars(self.args), "s": s[agent_id].tolist(), "evaluate": False}, cls=NumpyEncoder)
-                    task = comm.isend(s[agent_id], dest=agent_id+1, tag=0)
+                 # Get action from every agent
                 requests = []
                 for agent_id in range(self.args.n_agents):
-                    req = comm.irecv(u[agent_id], source=agent_id+1, tag=1)
+                    # task = app.send_task("worker.get_action", queue='q' + str(agent_id), kwargs={"agent_id": agent_id, "args": vars(self.args), "s": s[agent_id].tolist(), "evaluate": False}, cls=NumpyEncoder)
+                    req = comm.Isend(s[agent_id], dest=agent_id+1, tag=0)
                     requests.append(req)
                 for req in requests:
                     req.wait()
+
+                # requests = []
+                # for agent_id in range(self.args.n_agents):
+                #     req = comm.Irecv(u[agent_id], source=agent_id+1, tag=1)
+                #     requests.append(req)
+                # for req in requests:
+                #     req.wait()
+                requests = []
+                for agent_id in range(self.args.n_agents):
+                    req = comm.Irecv(u[agent_id], source=agent_id+1, tag=1)
+                    requests.append(req)
+                for i in range(self.args.n_agents):
+                    requests[i].wait()
 
                 for action in u:
                     actions.append(action)
@@ -87,24 +98,28 @@ class Runner:
                         o_next.append(transitions['o_next_%d' % agent_id])
 
                     # Send o_next to each agent and get their target network's next action u_next
-                    for agent_id in range(self.args.n_agents):
-                        # task = app.send_task("worker.get_target_next_action", queue='q' + str(agent_id), kwargs={"agent_id": agent_id, "args": vars(self.args), "s": o_next[agent_id].tolist()}, cls=NumpyEncoder)
-                        req = comm.isend(o_next[agent_id], dest=agent_id+1, tag=2)
                     requests = []
                     for agent_id in range(self.args.n_agents):
-                        req = comm.irecv(u_next[agent_id], source=agent_id+1, tag=3)
+                        # task = app.send_task("worker.get_target_next_action", queue='q' + str(agent_id), kwargs={"agent_id": agent_id, "args": vars(self.args), "s": o_next[agent_id].tolist()}, cls=NumpyEncoder)
+                        req = comm.Isend(o_next[agent_id], dest=agent_id+1, tag=2)
                         requests.append(req)
                     for req in requests:
                         req.wait()
 
-                    # Send u_next to each agent to train
-                    u_next = np.array(u_next)
                     requests = []
                     for agent_id in range(self.args.n_agents):
-                        req =comm.isend(u_next, dest=agent_id+1, tag=4)
+                        req = comm.Irecv(u_next[agent_id], source=agent_id+1, tag=3)
                         requests.append(req)
-                    for req in requests:
-                        req.wait()
+                    for i in range(self.args.n_agents):
+                        requests[i].wait()
+
+                    # Send u_next to each agent to train
+                    requests = []
+                    for agent_id in range(self.args.n_agents):
+                        req =comm.Isend(u_next, dest=agent_id+1, tag=4)
+                        requests.append(req)
+                    for i in range(self.args.n_agents):
+                        requests[i].wait()
                     
                 if time_step > 0 and time_step % self.args.evaluate_rate == 0:
                     returns.append(self.evaluate())
@@ -119,26 +134,29 @@ class Runner:
                 self.epsilon = max(0.05, self.epsilon - 0.0000005)
         # Worker
         else:   
-            if rank in [1, 2, 3]:
-                agent_id = rank - 1
+            agent_id = rank - 1
 
-                req = comm.irecv(s[agent_id], source=0, tag=0)
+            req = comm.Irecv(s[agent_id] , source=0, tag=0)
+            req.wait()
+            
+            with torch.no_grad():
+                action = self.agents[agent_id].select_action(s[agent_id], self.args.noise_rate, self.args.epsilon)
+                req = comm.Isend(action, dest=0, tag=1)
                 req.wait()
-                
-                u[agent_id] = self.agents[agent_id].select_action(s, self.args.noise_rate, self.args.epsilon)
-                req = comm.isend(u[agent_id], dest=0, tag=1)
-                
-                req = comm.irecv(o_next[agent_id], source=0, tag=2)
+            
+            req = comm.Irecv(o_next[agent_id], source=0, tag=2)
+            req.wait()
+
+            with torch.no_grad():
+                action_next = self.agents[agent_id].policy.actor_target_network(torch.tensor(o_next[agent_id], dtype=torch.float))
+                req = comm.Isend(action_next, dest=0, tag=3)
                 req.wait()
 
-                u_next[agent_id] = self.agents[agent_id].policy.actor_target_network(torch.tensor(o_next[agent_id], dtype=torch.float))
-                req = comm.isend(u_next[agent_id], dest=0, tag=3)
+            u_next = np.array(u_next)
 
-                u_next = np.array(u_next)
-
-                req = comm.irecv(u_next, source=0, tag=4)
-                req.wait()
-                self.agents[agent_id].learn(transitions, torch.tensor(u_next, dtype=torch.float))
+            req = comm.Irecv(u_next, source=0, tag=4)
+            req.wait()
+            self.agents[agent_id].learn(transitions, torch.tensor(u_next, dtype=torch.float))
 
 
 
