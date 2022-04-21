f4ecaaad31d38823e543b5960cbc60b207ff6eee
Xinda Zhou <xindaz@weh5201-b3.andrew.cmu.edu>
2022-04-21 03:03:48 -0400

    2million steps maddpg adversary

--- common/model_cleanup_script.py
+++ common/model_cleanup_script.py
@@ -1,15 +1,18 @@
 import os
 
-model_dir = "../model/simple_tag/worker_3/"
+model_dirs = ["../model/simple_tag/worker_0/", "../model/simple_tag/worker_1/", 
+              "../model/simple_tag/worker_2/", "../model/simple_tag/worker_3/"]
 
-agent_dirs = []
 
-for dir in os.listdir(model_dir):
-    if dir[:5] == "agent":
-        agent_dirs.append(model_dir + dir)
+for model_dir in model_dirs:
+    agent_dirs = []
 
-for dir in agent_dirs:
-    for filename in os.listdir(dir):
-        num = int(filename.split("_")[0])
-        if (num + 1) % 50 != 0:
-            os.remove(dir + "/" + filename)
+    for dir in os.listdir(model_dir):
+        if dir[:5] == "agent":
+            agent_dirs.append(model_dir + dir)
+
+    for dir in agent_dirs:
+        for filename in os.listdir(dir):
+            num = int(filename.split("_")[0])
+            if (num + 1) % 50 != 0:
+                os.remove(dir + "/" + filename)
